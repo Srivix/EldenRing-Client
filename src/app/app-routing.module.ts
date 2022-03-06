fdc11bfa4285b359740855f392d28aed50a5d5b3
Ivan Ciudad Espinar <ivanciudad98@gmail.com>
2022-03-06 22:50:01 +0100

    Adding Styles and Responsive Web Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { COMPILER_OPTIONS, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeaponListComponent } from './weapon/weapon-list/weapon-list.component';
 
 const routes: Routes = [
+  { path: '', redirectTo:'', pathMatch: 'full'},
   { path: 'weapons', component: WeaponListComponent },
 ];
 
